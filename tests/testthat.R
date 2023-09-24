c39e174d73589a68352ac3746567c6e6d609cbda
JohannaMz <johanna@maertz.eu>
2023-09-24 16:20:57 +0200

    testing setup

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,13 +1,7 @@
-# This file is part of the standard setup for testthat.
-# It is recommended that you do not modify it.
-#
-# Where should you do additional test configuration?
-# Learn more about the roles of various files in:
-# * https://r-pkgs.org/tests.html
-# * https://testthat.r-lib.org/reference/test_package.html#special-files
+Sys.setenv("R_TESTS" = "")
 
 library(testthat)
 library(ClusterApp)
 
 test_check("ClusterApp")
-Sys.setenv("R_TESTS" = "")
+
